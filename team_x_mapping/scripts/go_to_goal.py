c53d0d42108c56c7e67eadef46c251f6e6f61840
dbdxwyn@163.com <dbdxwyn@163.com>
2022-04-02 14:53:57 -0400

    0402 change to map frame

--- team_x_mapping/scripts/go_to_goal.py
+++ team_x_mapping/scripts/go_to_goal.py
@@ -6,8 +6,6 @@
 # Mar 23, 2022
 
 
-
-# todo: Functionality of stop 2s at each way point
 # todo: Current publish goals in "odom" frame, not sure if we must publish in "map" frame
 
 
@@ -15,9 +13,8 @@
 #  ros
 import rospy
 from nav_msgs.msg import Odometry
-from actionlib_msgs.msg import GoalStatusArray
 from geometry_msgs.msg import PoseStamped
-
+from move_base_msgs.msg import MoveBaseActionFeedback
 #
 import numpy as np
 import math
@@ -35,6 +32,11 @@ wp_idx = 0
 odom_pos = [0,0,0]
 odom_ang =  0
 timestamp = 0
+
+# map
+map_pos = [0,0,0]
+map_ang = 0
+
 
 pub = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=10)
 goal = PoseStamped()
@@ -63,23 +65,43 @@ def qua2rpy(qua):
 
 
 def odom_callback(msg):
-    global odom_pos
-    global odom_ang
-    global goal
-
-    odom_pos[0] = (msg.pose.pose.position.x)
-    odom_pos[1] = (msg.pose.pose.position.y)
-    odom_pos[2] = (msg.pose.pose.position.z)
-
-    odom_qua = [0,0,0,0]
-    odom_qua[0] = (msg.pose.pose.orientation.x)
-    odom_qua[1] = (msg.pose.pose.orientation.y)
-    odom_qua[2] = (msg.pose.pose.orientation.z)
-    odom_qua[3] = (msg.pose.pose.orientation.w)
-    odom_ang = qua2rpy(odom_qua)
+    # global odom_pos
+    # global odom_ang
+    # global goal
+    #
+    # odom_pos[0] = (msg.pose.pose.position.x)
+    # odom_pos[1] = (msg.pose.pose.position.y)
+    # odom_pos[2] = (msg.pose.pose.position.z)
+    #
+    # odom_qua = [0,0,0,0]
+    # odom_qua[0] = (msg.pose.pose.orientation.x)
+    # odom_qua[1] = (msg.pose.pose.orientation.y)
+    # odom_qua[2] = (msg.pose.pose.orientation.z)
+    # odom_qua[3] = (msg.pose.pose.orientation.w)
+    # odom_ang = qua2rpy(odom_qua)
 
     update_goal()
 
+""" update pose data in map frame"""
+
+
+def map_callback(msg):
+    global map_pos
+    global map_ang
+    global goal
+
+    map_pos[0] = (msg.feedback.base_position.pose.position.x)
+    map_pos[1] = (msg.feedback.base_position.pose.position.y)
+    map_pos[2] = (msg.feedback.base_position.pose.position.z)
+
+    map_qua = [0,0,0,0]
+    map_qua[0] = (msg.feedback.base_position.pose.orientation.x)
+    map_qua[1] = (msg.feedback.base_position.pose.orientation.y)
+    map_qua[2] = (msg.feedback.base_position.pose.orientation.z)
+    map_qua[3] = (msg.feedback.base_position.pose.orientation.w)
+    map_ang = qua2rpy(map_qua)
+
+    # update_goal()
 
 """ 
 get waypoint goal 
@@ -90,8 +112,9 @@ def get_waypoints():
     global wp_list
     global wp_num
     # Uncomment line below when running on robot
-    # with open('/home/allen/catkin_ws/src/team_x_mapping/global_waypoints.txt', 'r') as f:
-    with open('/home/pran/catkin_ws/src/Robotics-Research-Assignments/team_x_mapping/global_waypoints.txt', 'r') as f:
+    with open('/home/allen/catkin_ws/src/team_x_mapping/global_waypoints.txt', 'r') as f:
+    # with open('/home/allen/catkin_ws/src/team_x_mapping/global-wp-2.txt', 'r') as f:
+    # with open('/home/pran/catkin_ws/src/Robotics-Research-Assignments/team_x_mapping/global_waypoints.txt', 'r') as f:
         lines = f.readlines()
         f.close()
     wp_num = len(lines)
@@ -110,10 +133,15 @@ Check the robot arrival status
 
 def check_arrive():
     global wp_list
-    global odom_pos
-    global odom_ang
     global wp_idx
     global wp_num
+    global odom_pos
+    global odom_ang
+    global map_pos
+    global map_ang
+
+    current_pos = map_pos
+    current_ang = map_ang
 
     if wp_idx >= wp_num:
         return False
@@ -121,21 +149,14 @@ def check_arrive():
     ang = qua2rpy([wp_list[wp_idx][3],wp_list[wp_idx][4],wp_list[wp_idx][5],wp_list[wp_idx][6]])
 
     # check if arrive, please keep following parameters are the same as them in dwa_local_planner_params.yaml
-    if  abs(wp_list[wp_idx][0] - odom_pos[0]) < 0.17 and \
-        abs(wp_list[wp_idx][1] - odom_pos[1]) < 0.17 and \
-        abs(wp_list[wp_idx][2] - odom_pos[2]) < 0.17 and \
-        abs(ang - odom_ang) < 4: # degree (0-360)
+    if  abs(wp_list[wp_idx][0] - current_pos[0]) < 0.10 and \
+        abs(wp_list[wp_idx][1] - current_pos[1]) < 0.10 and \
+        abs(wp_list[wp_idx][2] - current_pos[2]) < 0.03 and \
+        abs(ang - current_ang) < 4: # degree (0-360)
 
         return True
 
     return False
-
-# def check_arrive(msg):
-#     global goal_reached
-#     if msg.status_list[0].status == 3: # goal reached
-#         goal_reached  = True
-#     else:
-#         goal_reached  = False
 
 """
 Update way point goal
@@ -148,10 +169,6 @@ def update_goal():
     global start_time
 
     if check_arrive() is True:
-    # if goal_reached:
-        # sleep 2 sec
-        # rospy.Duration(2.0).sleep()
-        # arrive_flag = False
         rospy.loginfo("Goal %d has arrived", wp_idx)
         # update way point
         start_time = time.time()       
@@ -159,12 +176,13 @@ def update_goal():
         if wp_idx >= wp_num:
             rospy.loginfo("TASK FINISHED! NO MORE WAY POINTS.")
 
-        #  if still have way points in the list
+    #  if still have way points in the list
     if wp_idx < wp_num:
         # update goal
         if (time.time() - start_time > 2):
             rospy.loginfo_throttle(3, "Adding new goal!")
-            goal.header.frame_id = "odom"
+            goal.header.frame_id = "map"
+            # goal.header.frame_id = "odom"
             goal.pose.position.x = wp_list[wp_idx][0]
             goal.pose.position.y = wp_list[wp_idx][1]
             # goal.pos.position.z = wp_list[wp_idx][2]
@@ -197,6 +215,7 @@ def init():
     rospy.init_node('go_to_goal', anonymous=True)
 
     sub = rospy.Subscriber('/odom', Odometry, odom_callback)
+    sub2 = rospy.Subscriber('/move_base/feedback',MoveBaseActionFeedback, map_callback)
     # sub = rospy.Subscriber('/move_base/status', GoalStatusArray, check_arrive)
     pub_goal()
 
